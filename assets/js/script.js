d4aceed7c896be1e39dc5f99f7f28338b4483ea6
unknown <mhalder9555@gmail.com>
2023-10-11 22:23:46 -0500

    Added functionality to display questions one after another after an answer is clicked, and to track whether the clicked answer was correct.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+var quizBox = document.querySelector(".quiz-box");
 var startMenu = document.querySelector(".start-menu");
 var startBtn = document.querySelector(".start-btn");
 var gameOverMenu = document.querySelector(".game-over");
@@ -11,12 +12,12 @@ var currentQuestionIndex = 0;
 var question1 = {
   title: "What is used to store data in JavaScript with the keyword 'var'?",
   answers: ["A variable", "A declaration", "A function"],
-  correctAnswerIndex: 0
+  correctAnswerIndex: "0"
 };
 var question2 = {
   title: "What is NOT a primitive data type in JavaScript?",
   answers: ["boolean", "object", "number"],
-  correctAnswerIndex: 1
+  correctAnswerIndex: "1"
 }
 var questions = [question1, question2];
 
@@ -25,7 +26,8 @@ console.log(questions);
 function changeStartMenuDisplay() {
   if (isStartVisible) {
     startMenu.setAttribute("style", "display: none");
-    gameOverMenu.setAttribute("style", "display: block");
+    displayQuestion(question1);
+    // gameOverMenu.setAttribute("style", "display: block");
     isStartVisible = false;
   } else if (!isStartVisible) {
     startMenu.setAttribute("style", "display: block");
@@ -44,9 +46,65 @@ function changeStartMenuDisplay() {
 
 */
 
-function displayQuiz(question) {
+function displayQuestion(question) {
+  // var previousQuestionDiv = document.querySelector(".question-div");
+  // previousQuestionDiv.remove();
 
+  var questionDiv = document.createElement("div");
+  questionDiv.setAttribute("class", "question-div");
+  quizBox.appendChild(questionDiv);
+  var questionTitle = document.createElement("h2");
+  questionTitle.textContent = question.title;
+  questionDiv.appendChild(questionTitle);
+  var questionList = document.createElement("ul");
+  questionList.setAttribute("style", "list-style-type: none")
+  questionDiv.appendChild(questionList);
+
+  for (var i = 0; i < question.answers.length; i++) {
+    var answer = document.createElement("li");
+    answer.setAttribute("class", "answer-btn");
+    answer.setAttribute("id", `${i}`);
+    answer.textContent = question.answers[i];
+    // answer.setAttribute();
+    questionList.appendChild(answer);
+  }
+}
+
+function checkGameOver() {
+  if (currentQuestionIndex === questions.length - 1) {
+    return true;
+  }
+}
+
+function checkSelection(event) {
+  if (event.target.matches(".answer-btn")) {
+    var clickedAnswerID = event.target.id;
+
+    if (clickedAnswerID !== questions[currentQuestionIndex].correctAnswerIndex) {
+      console.log("Wrong Answer.");
+    }
+    // console.log(clickedAnswerID);
+    // console.log(questions[currentQuestionIndex].correctAnswerIndex);
+
+    // console.log(event.target.id);
+    var previousQuestionDiv = document.querySelector(".question-div");
+    previousQuestionDiv.remove();
+
+
+
+    if (checkGameOver()) {
+      console.log("Game Over.");
+
+    } else {
+      currentQuestionIndex++;
+      displayQuestion(questions[currentQuestionIndex]);
+    }
+
+    // currentQuestionIndex++;
+    // displayQuestion(questions[currentQuestionIndex]);
+  }
 }
 
 startBtn.addEventListener("click", changeStartMenuDisplay);
 startOverBtn.addEventListener("click", changeStartMenuDisplay);
+quizBox.addEventListener("click", checkSelection);
